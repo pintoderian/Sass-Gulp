9ac7d34aafee565b406677ddcfc721a7116271dd
Derian Pinto <pintoderian@gmail.com>
2018-01-03 12:04:10 -0500

    Galería con Mustache

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -6,8 +6,13 @@ window.jQuery = require('jquery');
 bootstrap = require('bootstrap');
 Mustache = require('mustache');
 
-jQuery(document).ready(function(){
-    msg();
+jQuery(document).ready(function($){
+    var xd = $.getJSON('data.json',function(){
+    }).done(function(data){
+        var template = $('#template').html();
+        var mostrarTemplate = Mustache.render(template, data);
+        $('#galeria-londres').html(mostrarTemplate);
+    });
 });
 },{"bootstrap":2,"jquery":3,"mustache":4}],2:[function(require,module,exports){
 /*!
--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ window.jQuery = require('jquery');
 bootstrap = require('bootstrap');
 Mustache = require('mustache');
 
-jQuery(document).ready(function(){
-    msg();
+jQuery(document).ready(function($){
+    var xd = $.getJSON('data.json',function(){
+    }).done(function(data){
+        var template = $('#template').html();
+        var mostrarTemplate = Mustache.render(template, data);
+        $('#galeria-londres').html(mostrarTemplate);
+    });
 });
